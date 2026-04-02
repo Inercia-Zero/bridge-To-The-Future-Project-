54d407385f1c09672e4f57eba52dad585a4f87b1
Mesquita <iagobr.386@gmail.com>
2026-04-02 19:30:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 import streamlit as st
-st.title("bridge to the future")
-st.write("projeto educaional para estudantes da rede pública.")
+
+from theme import apply.theme
+from mentors import MENTORS
+from database import init_db
+from prompts import build_prompt
+from groq_client import ask_ai
+
+#Configuração
+st.set_page_confi(page_tilte="Brigde to the future", layout="wide")
+
+#Tema
+apply_theme()
+
+#Banco
+init_db()
+
+#interface
+st.title("bridge to the future 🚀")
+
+user_imput = st.text_input("digite sua dúvida:")
+
+if user_input:
+  prompt = build_prompt(user_input, mentor)
+  resposta = ask_ai(prompt)
+  
